25816c26e5fa3dacc66363e01bbf08d2a3d96143
mxk489 <30409988+mxk489@users.noreply.github.com>
2017-08-27 21:48:05 -0400

    Add files via upload

---
+++ code/managersignup.php
@@ -0,0 +1,181 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="styles/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-teal.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+
+
+</style>
+
+    <!-- Navbar -->
+<div class="w3-top">
+ <div class="w3-bar w3-theme-d2 w3-left-align">
+  <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-hover-white w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
+  <a href="managerhome.php" class="w3-bar-item w3-button w3-teal"><i class="fa fa-home w3-margin-right"></i>Saheb</a>
+  
+  <!--<a href="#Claim_Your Profile" class="w3-bar-item w3-button #009688">Claim Your Profile</a>-->
+  
+  <a href="managerhome.php" class="w3-right w3-bar-item w3-button #009688">Sign In</a>
+  
+ 
+  <!--  <div class="w3-dropdown-hover w3-hide-small">
+    <button class="w3-button" title="Notifications">Dropdown <i class="fa fa-caret-down"></i></button>     
+    <div class="w3-dropdown-content w3-card-4 w3-bar-block">
+      <a href="#" class="w3-bar-item w3-button">Link</a>
+      <a href="#" class="w3-bar-item w3-button">Link</a>
+      <a href="#" class="w3-bar-item w3-button">Link</a>-->
+   
+      </div>
+   <!--</div>
+  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Search"><i class="fa fa-search"></i></a>
+ </div>-->
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+
+        <!-- Page Container -->
+<div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">       
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    <a class="w3-right w3-bar-item w3-button #009688"> <h7>&nbsp;</h7></a>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Center Column -->
+    <div class="w3-third">
+    
+      <!DOCTYPE html>
+<html>
+<style>
+form {
+    border: 3px solid #f1f1f1;
+}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+.cancelbtn {
+    width: auto;
+    padding: 10px 18px;
+    background-color: #f44336;
+}
+
+.login {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    font-weight: bold;
+    
+}
+
+img.avatar {
+    width: 40%;
+    border-radius: 50%;
+}
+
+.container {
+    padding: 16px;
+}
+
+span.psw {
+    float: right;
+    
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+    span.psw {
+       display: block;
+       float: none;
+    }
+    .cancelbtn {
+       width: 100%;
+    }
+
+    
+    
+    
+}
+</style>
+<body>
+
+
+<form action="/action_page.php">
+  <div class="login">
+   
+     <p class="w3-large w3-text-teal ">Get Started - It is Free!</p>
+  </div>
+
+  <div class="container w3-round">
+    
+    <input type="text" placeholder="First Name" name="mk_managerfirst" required>
+
+    <input type="text" placeholder="Last Name" name="mk_managerlast" required>
+
+    <input type="text" placeholder="Email" name="mk_manageremail" required>
+
+    <input type="password" placeholder="Password" name="mk_managerpassword" required>
+    
+    <p class="w3-small w3-text-grey">By clicking Join Now, you <a href="#">agree</a> to our User Agreement, Privacy Policy, and Cookie Policy.</p>
+           
+    <button type="submit">Join Now</button>
+    <input type="checkbox" checked="checked"> Remember me
+     </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+     
+  </div>
+</form>
+
+</body>
+      
+
+    <!-- End Center Column -->
+
+<!-- Right Column -->
+    <div class="w3-third">
+    <a class="w3-right w3-bar-item w3-button #009688"> <h7>&nbsp;</h7></a>
+
+    <!-- End Right Column -->
+    </div>        
+  
+  <!-- End Page Container -->
+</div>
+        
+    </body>
+</html>
